dac72db4429e73ad2e915c682f71c0f2f5a97b35
Tim Hagemann <admin@infinityblade.de>
2014-10-04 11:26:43 +0200

    Added mooshroom

---
+++ Implementation/src/main/ruby/entities/remote_mooshroom.rb
@@ -0,0 +1,27 @@
+require '../base'
+require '../namespace'
+
+module RemoteEntities
+	module Entities
+		class RemoteMooshroomImpl < BaseAttackingEntity
+			include RemoteEntities::RemoteMooshroom
+		end
+
+		class RemoteMooshroomEntity < NMS::EntityMushroomCow
+			extend RemoteEntities::EntityMixins::EntityHandle
+			include RemoteEntities::EntityMixins::RemoteMethodDefaults
+
+			def self.default_movement_desires
+				[
+						# TODO
+				]
+			end
+
+			def self.default_target_desires
+				[
+						# TODO
+				]
+			end
+		end
+	end
+end
