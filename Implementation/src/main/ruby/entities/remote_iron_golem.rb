833cafca7c4502c3bde6a23de61a225b9249613f
Tim Hagemann <admin@infinityblade.de>
2014-10-04 11:45:43 +0200

    Added iron golem

---
+++ Implementation/src/main/ruby/entities/remote_iron_golem.rb
@@ -0,0 +1,27 @@
+require '../base'
+require '../namespace'
+
+module RemoteEntities
+	module Entities
+		class RemoteSlimeImpl < BaseAttackingEntity
+			include RemoteEntities::Entities::RemoteIronGolem
+		end
+
+		class RemoteIronGolemEntity < NMS::EntityIronGolem
+			extend RemoteEntities::EntityMixins::EntityHandle
+			include RemoteEntities::EntityMixins::RemoteMethodDefaults
+
+			def self.default_movement_desires
+				[
+						# TODO
+				]
+			end
+
+			def self.default_target_desires
+				[
+						# TODO
+				]
+			end
+		end
+	end
+end
