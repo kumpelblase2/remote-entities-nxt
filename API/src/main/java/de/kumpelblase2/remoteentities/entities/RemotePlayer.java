edb90bf527663bd02245c6533d97af079e93b896
Tim Hagemann <tim@infinityblade.de>
2014-11-01 00:13:39 +0100

    Add API methods to player.

--- API/src/main/java/de/kumpelblase2/remoteentities/entities/RemotePlayer.java
+++ API/src/main/java/de/kumpelblase2/remoteentities/entities/RemotePlayer.java
@@ -1,7 +1,13 @@
 package de.kumpelblase2.remoteentities.entities;
 
+import org.bukkit.Location;
 import de.kumpelblase2.remoteentities.api.RemoteEntity;
 
 public interface RemotePlayer extends RemoteEntity
 {
+	public boolean enterBed(Location inLocation);
+	public void leaveBed();
+	public boolean isSleeping();
+	public void doArmSwing();
+	public void fakeDamage();
 }
